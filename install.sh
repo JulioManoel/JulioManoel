2eaf9557d94b1dd4c940a43e9bfce763c4cceff9
Julio Cesar Bonow Manoel <84098213+JulioManoel@users.noreply.github.com>
2023-09-06 10:16:15 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt install lsb-core
-sudo apt install lsb
-sudo apt update
-sudo apt upgrade -f
-sudo apt install curl
-bash <(curl -s https://raw.githubusercontent.com/LiquidGalaxyLAB/liquid-galaxy/master/install.sh)
